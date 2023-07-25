f7af1cc322ee209f2bb0da472fb062134de78a73
Logan Matheny <loganpaulmatheny@gmail.com>
2023-07-25 08:26:26 -0400

    Create basic currentColorPalette

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+// ===== GLOBAL VARIABLES =====
+var currentColorPalette = [];
+
+// ===== EVENT LISTENERS =====
 window.addEventListener("load", function () {
-  randomHexCode();
+  setCurrentColors(currentColorPalette);
 });
 
 function randomHex() {
@@ -8,10 +12,34 @@ function randomHex() {
 }
 
 function randomHexCode() {
-  var randHexCode = "";
+  var randHexCode = "#";
   for (let i = 0; i <= 5; i++) {
     var character = randomHex();
     randHexCode += character;
   }
   return randHexCode;
+}
+
+function createColor() {
+  var colorCode = randomHexCode();
+  var color = {
+    hexCode: colorCode,
+    status: "locked",
+    id: Date.now(),
+  };
+  return color;
+}
+
+function setCurrentColors(currentColorPalette, savedColorPalette = []) {
+  if (savedColorPalette.length) {
+    currentColorPalette = savedColorPalette;
+  } else {
+    if (currentColorPalette.length === 0) {
+      for (let i = 0; i <= 5; i++) {
+        currentColorPalette.push(createColor());
+      }
+    }
+  }
+  //   console.log(currentColorPalette);
+  return currentColorPalette;
 }
